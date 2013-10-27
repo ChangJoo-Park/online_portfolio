29388a6f949dbfd99aba933c9bd96429842ad583
Chang Ju Park <pcjpcj2@gmail.com>
2013-10-28 01:12:32 +0900

    #35 - 삭제 구역에 프로젝트 아이템을 올리면 삭제

--- app/assets/javascripts/sort_project.js
+++ app/assets/javascripts/sort_project.js
@@ -1,16 +1,49 @@
 $(function() {
-  return $('#projects').sortable({
+
+   $('.droppable').droppable({
+    accept: ".sortable li",
+    tolerance: 'intersect',
+
+    over: function(event,ui){
+      $('.project-placeholder').html("<span>Remove This</span>").css("background","red");
+      ui.draggable.css("opacity","0.9");
+    },
+
+    out: function(event,ui){
+      $('.project-placeholder').html("").css("background","#eee");
+    },
+
+    drop: function(event,ui){
+      var result = confirm("Are you sure?");
+      if(result){
+        ui.draggable.remove();
+        $('.project-placeholder').fadeOut();
+        var id = ui.draggable.attr('id').replace('project_','');
+        return $.post('/projects/'+id, "_method=delete");
+      }else{
+        $('.project-placeholder').html("").css("background","#eee");
+      }
+
+    }
+  });
+
+  $('.sortable').sortable({
     handle: '.handle',
     opacity: 0.8,
+    placeholder: 'project-placeholder',
+    revert: true,
+    connectWith: '.droppable',
     start: function(e, ui){
-      $(ui.placeholder).hide(500);
+      //$(ui.placeholder).hide(500);
+      //$(".trashcan").show(500);
     },
     stop: function(e, ui){
-      $(ui.placeholder).show(500);
+      //$(ui.placeholder).show(500);
+      //(".trashcan").hide(500);
     },
     update: function() {
       return $.post($(this).data('update-url'), $(this).sortable('serialize'));
     }
-  });
-  $('#projects').disableSelection();
+  }).disableSelection();;
+
 });
