318d9123fe5f835f45762c68d4d851fa6dd4a46f
Chang Ju Park <pcjpcj2@gmail.com>
2013-10-27 18:02:59 +0900

    #5 프로젝트 목록 디자인 소팅시 유지하도록해야함

--- app/assets/javascripts/sort_project.js
+++ app/assets/javascripts/sort_project.js
@@ -1,9 +1,16 @@
 $(function() {
   return $('#projects').sortable({
     handle: '.handle',
-    opacity: 0.4,
+    opacity: 0.8,
+    start: function(e, ui){
+      $(ui.placeholder).hide(500);
+    },
+    stop: function(e, ui){
+      $(ui.placeholder).show(500);
+    },
     update: function() {
       return $.post($(this).data('update-url'), $(this).sortable('serialize'));
     }
   });
+  $('#projects').disableSelection();
 });
