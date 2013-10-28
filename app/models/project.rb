80742d50f1f5a975a0b6a0cba04871bd945c7b3d
Chang Ju Park <pcjpcj2@gmail.com>
2013-10-29 02:01:42 +0900

    #37 프로젝트 생성시 기본값을 추가하여 best_in_place 에서 nil 이 작동하지않음, 새로고침을 할경우에는 됨

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,17 @@ class Project < ActiveRecord::Base
 	has_many :sub_images, dependent: :destroy
 	acts_as_list
 	validates :title, presence: true
-end 
+  #FIXME will be fixed best_in_place 'nil'
+  after_initialize :default_values
+
+
+  private
+
+    def default_values
+      self.description ||= 'Input Something..'
+      self.url ||= 'Input Something..'
+      self.work_for ||= 'Input Something..'
+      self.types ||= 'Input Something..'
+      self.layouts ||= 'layout1'
+    end
+end
